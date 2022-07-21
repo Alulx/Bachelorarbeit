1838aeff243aaaf38db2d10e1f5241dbffe4ef41
Alexander Hultzsch <Alexander.Hultzsch@t-systems.com>
2022-07-21 22:14:29 +0200

    init contract

---
+++ Backend/main.ts
@@ -0,0 +1,24 @@
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+
+const SBT_ABI = require( './SBT.json');
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+main();
+/**
+ *  The Main function;
+ */
+async function main(): Promise<void> {
+  console.log('Starting DeRep...');
+
+  const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+  console.log(await web3.eth.net.isListening());
+
+  console.log(await web3.eth.getBalance('0x70997970C51812dc3A010C7d01b50e0d17dc79C8'));
+  const sbt =  new web3.eth.Contract(SBT_ABI.abi, contractAddress); //  ?????
+  
+  //sbt.methods.hasSoul('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+
+}
+
