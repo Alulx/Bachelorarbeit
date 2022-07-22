a87be32e3555257642dfa029be924d965087cb88
Alexander Hultzsch <Alexander.Hultzsch@t-systems.com>
2022-07-22 10:15:59 +0200

    MINIMUM VIABLE PRODUCT

--- Backend/main.ts
+++ Backend/main.ts
@@ -1,8 +1,7 @@
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-
-const SBT_ABI = require( './SBT.json');
+import SBT_ABI from './SBT.json';
 const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 main();
@@ -16,9 +15,9 @@ async function main(): Promise<void> {
   console.log(await web3.eth.net.isListening());
 
   console.log(await web3.eth.getBalance('0x70997970C51812dc3A010C7d01b50e0d17dc79C8'));
-  const sbt =  new web3.eth.Contract(SBT_ABI.abi, contractAddress); //  ?????
-  
-  //sbt.methods.hasSoul('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266');
+  const sbt =  new web3.eth.Contract(SBT_ABI.abi as AbiItem[], contractAddress); //  ?????
+  console.log(await sbt.methods.hasSoul('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266').call());
+  console.log(await web3.eth.Contract.defaultAccount);
 
 }
 
