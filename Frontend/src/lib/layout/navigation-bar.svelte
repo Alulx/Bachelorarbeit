70d12e27927a152d2163cc55397ecee14808469c
Alulx <alexanderhultzsch@web.de>
2022-08-05 23:14:48 +0200

    added new color/theme

--- Frontend/src/lib/layout/navigation-bar.svelte
+++ Frontend/src/lib/layout/navigation-bar.svelte
@@ -3,7 +3,7 @@
 import TextInput from "$lib/universal/TextInput.svelte";
 
 </script>
-<div class="navbar bg-secondary w-full">
+<div class="navbar bg-primary w-full">
     <p class="btn btn-ghost normal-case text-xl">Decentralized Reputation</p>
     <div  style="margin-left: auto;  margin-right: 0;">
         <TextInput> </TextInput>
