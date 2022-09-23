b185445688bf9901c67c12136d958b5c5c180fe6
alex <alexanderhultzsch@web.de>
2022-09-23 16:12:17 +0200

    local changes coming from the swap

--- Frontend/.svelte-kit/runtime/env/static/private.js
+++ Frontend/.svelte-kit/runtime/env/static/private.js
@@ -3,13 +3,16 @@
 export const LESSOPEN = "| /usr/bin/lesspipe %s";
 
 /** @type {import('$env/static/private').USER} */
-export const USER = "alhu";
+export const USER = "alex";
 
 /** @type {import('$env/static/private').npm_config_user_agent} */
-export const npm_config_user_agent = "npm/8.5.1 node/v17.6.0 linux x64 workspaces/false";
+export const npm_config_user_agent = "npm/7.21.1 node/v16.9.1 linux x64 workspaces/false";
+
+/** @type {import('$env/static/private').GIT_ASKPASS} */
+export const GIT_ASKPASS = "/home/alex/.vscode-server/bin/74b1f979648cc44d385a2286793c226e611f59e7/extensions/git/dist/askpass.sh";
 
 /** @type {import('$env/static/private').npm_node_execpath} */
-export const npm_node_execpath = "/usr/bin/node";
+export const npm_node_execpath = "/home/alex/.nvm/versions/node/v16.9.1/bin/node";
 
 /** @type {import('$env/static/private').SHLVL} */
 export const SHLVL = "1";
@@ -18,25 +21,31 @@ export const SHLVL = "1";
 export const npm_config_noproxy = "";
 
 /** @type {import('$env/static/private').HOME} */
-export const HOME = "/home/alhu";
+export const HOME = "/home/alex";
 
 /** @type {import('$env/static/private').OLDPWD} */
-export const OLDPWD = "/home/alhu/Bachelorarbeit";
+export const OLDPWD = "/home/alex/Bachelorarbeit";
 
 /** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
-export const TERM_PROGRAM_VERSION = "1.71.1";
+export const TERM_PROGRAM_VERSION = "1.71.2";
+
+/** @type {import('$env/static/private').NVM_BIN} */
+export const NVM_BIN = "/home/alex/.nvm/versions/node/v16.9.1/bin";
 
 /** @type {import('$env/static/private').VSCODE_IPC_HOOK_CLI} */
-export const VSCODE_IPC_HOOK_CLI = "/tmp/vscode-ipc-7818e3d1-a6ab-4437-bf01-a8f57fc803b4.sock";
+export const VSCODE_IPC_HOOK_CLI = "/tmp/vscode-ipc-ff2b4d4f-f453-472a-8257-af2c1426dd72.sock";
 
 /** @type {import('$env/static/private').npm_package_json} */
-export const npm_package_json = "/home/alhu/Bachelorarbeit/Frontend/package.json";
+export const npm_package_json = "/home/alex/Bachelorarbeit/Frontend/package.json";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_MAIN} */
+export const VSCODE_GIT_ASKPASS_MAIN = "/home/alex/.vscode-server/bin/74b1f979648cc44d385a2286793c226e611f59e7/extensions/git/dist/askpass-main.js";
+
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_NODE} */
+export const VSCODE_GIT_ASKPASS_NODE = "/home/alex/.vscode-server/bin/74b1f979648cc44d385a2286793c226e611f59e7/node";
 
 /** @type {import('$env/static/private').npm_config_userconfig} */
-export const npm_config_userconfig = "/home/alhu/.npmrc";
-
-/** @type {import('$env/static/private').npm_config_local_prefix} */
-export const npm_config_local_prefix = "/home/alhu/Bachelorarbeit/Frontend";
+export const npm_config_userconfig = "/home/alex/.npmrc";
 
 /** @type {import('$env/static/private').npm_config_engine_strict} */
 export const npm_config_engine_strict = "true";
@@ -51,40 +60,40 @@ export const WSL_DISTRO_NAME = "Ubuntu";
 export const COLOR = "1";
 
 /** @type {import('$env/static/private').NVM_DIR} */
-export const NVM_DIR = "/home/alhu/.nvm";
+export const NVM_DIR = "/home/alex/.nvm";
 
 /** @type {import('$env/static/private').npm_config_metrics_registry} */
 export const npm_config_metrics_registry = "https://registry.npmjs.org/";
 
 /** @type {import('$env/static/private').LOGNAME} */
-export const LOGNAME = "alhu";
+export const LOGNAME = "alex";
 
 /** @type {import('$env/static/private').NAME} */
-export const NAME = "w4deumsy9003565";
+export const NAME = "DESKTOP-NBN8EKQ";
 
 /** @type {import('$env/static/private').WSL_INTEROP} */
-export const WSL_INTEROP = "/run/WSL/11_interop";
+export const WSL_INTEROP = "/run/WSL/13_interop";
 
 /** @type {import('$env/static/private')._} */
-export const _ = "/usr/bin/npm";
+export const _ = "/home/alex/.nvm/versions/node/v16.9.1/bin/npm";
 
 /** @type {import('$env/static/private').npm_config_prefix} */
-export const npm_config_prefix = "/usr";
+export const npm_config_prefix = "/home/alex/.nvm/versions/node/v16.9.1";
 
 /** @type {import('$env/static/private').TERM} */
 export const TERM = "xterm-256color";
 
 /** @type {import('$env/static/private').npm_config_cache} */
-export const npm_config_cache = "/home/alhu/.npm";
+export const npm_config_cache = "/home/alex/.npm";
 
 /** @type {import('$env/static/private').npm_config_node_gyp} */
-export const npm_config_node_gyp = "/usr/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
+export const npm_config_node_gyp = "/home/alex/.nvm/versions/node/v16.9.1/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
 
 /** @type {import('$env/static/private').PATH} */
-export const PATH = "/home/alhu/Bachelorarbeit/Frontend/node_modules/.bin:/home/alhu/Bachelorarbeit/node_modules/.bin:/home/alhu/node_modules/.bin:/home/node_modules/.bin:/node_modules/.bin:/usr/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/home/alhu/.vscode-server/bin/e7f30e38c5a4efafeec8ad52861eb772a9ee4dfb/bin/remote-cli:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Program Files (x86)/Webex/Plugins:/mnt/c/Program Files (x86)/Intel/Intel(R) Management Engine Components/DAL:/mnt/c/Program Files/Intel/Intel(R) Management Engine Components/DAL:/mnt/c/Program Files/Git/cmd:/mnt/c/Program Files/nodejs/:/mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/ProgramData/DockerDesktop/version-bin:/mnt/c/Users/alhu/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/alhu/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Users/alhu/AppData/Local/GitHubDesktop/bin:/mnt/c/Users/alhu/AppData/Roaming/npm:/mnt/c/Users/alhu/AppData/Local/gitkraken/bin";
+export const PATH = "/home/alex/Bachelorarbeit/Frontend/node_modules/.bin:/home/alex/Bachelorarbeit/node_modules/.bin:/home/alex/node_modules/.bin:/home/node_modules/.bin:/node_modules/.bin:/home/alex/.nvm/versions/node/v16.9.1/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/home/alex/.vscode-server/bin/74b1f979648cc44d385a2286793c226e611f59e7/bin/remote-cli:/home/alex/.nvm/versions/node/v16.9.1/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Program Files (x86)/Common Files/Oracle/Java/javapath:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0/:/mnt/c/WINDOWS/System32/OpenSSH/:/mnt/c/Users/Alex/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/Alex/AppData/Local/Programs/Microsoft VS Code/bin:/mnt/c/Users/Alex/AppData/Local/Programs/MiKTeX/miktex/bin/x64/:/snap/bin";
 
 /** @type {import('$env/static/private').NODE} */
-export const NODE = "/usr/bin/node";
+export const NODE = "/home/alex/.nvm/versions/node/v16.9.1/bin/node";
 
 /** @type {import('$env/static/private').npm_package_name} */
 export const npm_package_name = "digitalx-mint-factory";
@@ -96,7 +105,7 @@ export const LANG = "C.UTF-8";
 export const LS_COLORS = "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:";
 
 /** @type {import('$env/static/private').VSCODE_GIT_IPC_HANDLE} */
-export const VSCODE_GIT_IPC_HANDLE = "/tmp/vscode-git-e17d6a8652.sock";
+export const VSCODE_GIT_IPC_HANDLE = "/tmp/vscode-git-708db66762.sock";
 
 /** @type {import('$env/static/private').TERM_PROGRAM} */
 export const TERM_PROGRAM = "vscode";
@@ -116,23 +125,26 @@ export const npm_lifecycle_event = "dev";
 /** @type {import('$env/static/private').LESSCLOSE} */
 export const LESSCLOSE = "/usr/bin/lesspipe %s %s";
 
+/** @type {import('$env/static/private').VSCODE_GIT_ASKPASS_EXTRA_ARGS} */
+export const VSCODE_GIT_ASKPASS_EXTRA_ARGS = "";
+
 /** @type {import('$env/static/private').npm_config_globalconfig} */
-export const npm_config_globalconfig = "/usr/etc/npmrc";
+export const npm_config_globalconfig = "/home/alex/.nvm/versions/node/v16.9.1/etc/npmrc";
 
 /** @type {import('$env/static/private').npm_config_init_module} */
-export const npm_config_init_module = "/home/alhu/.npm-init.js";
+export const npm_config_init_module = "/home/alex/.npm-init.js";
 
 /** @type {import('$env/static/private').PWD} */
-export const PWD = "/home/alhu/Bachelorarbeit/Frontend";
+export const PWD = "/home/alex/Bachelorarbeit/Frontend";
 
 /** @type {import('$env/static/private').npm_execpath} */
-export const npm_execpath = "/usr/lib/node_modules/npm/bin/npm-cli.js";
+export const npm_execpath = "/home/alex/.nvm/versions/node/v16.9.1/lib/node_modules/npm/bin/npm-cli.js";
 
 /** @type {import('$env/static/private').NVM_CD_FLAGS} */
 export const NVM_CD_FLAGS = "";
 
-/** @type {import('$env/static/private').npm_config_global_prefix} */
-export const npm_config_global_prefix = "/usr";
+/** @type {import('$env/static/private').XDG_DATA_DIRS} */
+export const XDG_DATA_DIRS = "/usr/local/share:/usr/share:/var/lib/snapd/desktop";
 
 /** @type {import('$env/static/private').npm_command} */
 export const npm_command = "run-script";
@@ -144,7 +156,7 @@ export const HOSTTYPE = "x86_64";
 export const WSLENV = "VSCODE_WSL_EXT_LOCATION/up";
 
 /** @type {import('$env/static/private').INIT_CWD} */
-export const INIT_CWD = "/home/alhu/Bachelorarbeit/Frontend";
+export const INIT_CWD = "/home/alex/Bachelorarbeit/Frontend";
 
 /** @type {import('$env/static/private').EDITOR} */
 export const EDITOR = "vi";
